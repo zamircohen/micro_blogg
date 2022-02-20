de85028945bd95066a3a5c8f187806c47dba1664
Zamir Cohen <90824610+zamircohen@users.noreply.github.com>
2022-02-20 14:38:46 +0100

    Fixed date stamp on posts

--- app.js
+++ app.js
@@ -1,25 +1,45 @@
-const bodyParser = require("body-parser")
 const express = require("express")
 const multer = require("multer")
 const path = require("path")
+const bodyParser = require("body-parser")
 
 const app = express()
 const PORT = 3000
-const STATIC_ROOT = path.join(__dirname, "static")
+const VIEWS_ROOT = path.join(__dirname, "views")
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+const POSTS = []
 
 const upload_photo = multer({dest: "user_photo" })
 
+
 app.get("/", (req, res) => {
-    res.sendFile("./index.html", { root: STATIC_ROOT })
+    res.render("./index.ejs", {root: VIEWS_ROOT})
 })
+
+app.get("/posts", (req, res) => {
+    res.render("./posts.ejs", { post_items: POSTS })
+})
+
 
 app.get("/create", (req, res) => {
-    res.sendFile("./create.html", { root: STATIC_ROOT })
+    res.render("./create.ejs", { root: VIEWS_ROOT })
 })
 
+
 app.get("/update", (req, res) => {
-    res.sendFile("./update.html", {root: STATIC_ROOT })
+    res.render("./update.ejs", {root: VIEWS_ROOT })
 })
+
+
+app.post("/submit", (req, res) => {
+    const { item } = req.body   
+    POSTS.push(item)
+    res.redirect("/posts")
+})
+
+
 
 
 // Upload user photo "function" connected to profile.html
@@ -27,7 +47,7 @@ app.use(upload_photo.single("file"))
 
 
 // app.post("/upload", (_req, res) => {
-//     res.redirect("./profile.html", {root: STATIC_ROOT })
+//     res.redirect("./profile.html", {root: VIEWS_ROOT })
 // })
 
 
