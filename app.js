8b73ccb19e9a7b826c36048e48ad006705a39f5c
Zamir Cohen <90824610+zamircohen@users.noreply.github.com>
2022-02-20 14:00:24 +0100

    Added posts.ejs and re-arranged files in folders

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const app = express()
 const PORT = 3000
 const STATIC_ROOT = path.join(__dirname, "static")
 
+const upload_photo = multer({dest: "user_photo" })
 
 app.get("/", (req, res) => {
     res.sendFile("./index.html", { root: STATIC_ROOT })
@@ -15,6 +16,21 @@ app.get("/", (req, res) => {
 app.get("/create", (req, res) => {
     res.sendFile("./create.html", { root: STATIC_ROOT })
 })
+
+app.get("/update", (req, res) => {
+    res.sendFile("./update.html", {root: STATIC_ROOT })
+})
+
+
+// Upload user photo "function" connected to profile.html
+app.use(upload_photo.single("file"))
+
+
+// app.post("/upload", (_req, res) => {
+//     res.redirect("./profile.html", {root: STATIC_ROOT })
+// })
+
+
 
 
 app.listen(PORT, () => {
