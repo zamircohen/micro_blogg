1b5afee5ff82dbc798ac888337dc306e69f5f599
Zamir Cohen <90824610+zamircohen@users.noreply.github.com>
2022-02-22 09:46:56 +0100

    Created signup function

--- app.js
+++ app.js
@@ -1,41 +1,74 @@
+// Packages
 const express = require("express")
+const mongoose = require("mongoose")
+const passport = require("passport")
+const session = require("express-session")
+const { User } = require("./models/user")
 const multer = require("multer")
-const path = require("path")
+// const path = require("path")
 const bodyParser = require("body-parser")
-const cookierParser = require("cookie-parser")
+// const cookierParser = require("cookie-parser")
 
+
+// Basic variables
 const app = express()
 const PORT = 3000
-const VIEWS_ROOT = path.join(__dirname, "views")
+// const VIEWS_ROOT = path.join(__dirname, "views")
+
+
+// Middlewares
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(session({
+    secret: "avsd1234",
+    resave: false,
+    saveUninitialized: false
+  }));
+app.use(passport.authenticate("session"));
 
 app.use(bodyParser.urlencoded({extended: true}))
-app.use(cookierParser())
+// app.use(cookierParser())
 
 
+// Program variables
 const POSTS = []
-
 const upload_photo = multer({dest: "user_photo" })
 
 
-app.get("/", (req, res) => {
-    res.render("./index.ejs", {root: VIEWS_ROOT})
+// Routing
+app.get("/login", (req, res) => {
+    res.render("./login.ejs")
 })
 
 app.get("/posts", (req, res) => {
-    res.render("./posts.ejs", { post_items: POSTS })
+    res.render("./posts.ejs")
 })
 
-
-app.get("/create", (req, res) => {
-    res.render("./create.ejs", { root: VIEWS_ROOT })
+app.get("/signup", (req, res) => {
+    res.render("./signup.ejs")
 })
-
 
 app.get("/update", (req, res) => {
     res.render("./update.ejs", {root: VIEWS_ROOT })
 })
 
 
+// CREATE NEW USER
+
+app.post("/signup", async (req, res) => {
+    const {username, password} = req.body;
+    const user = new User({username});
+    await user.setPassword(password);
+    await user.save();
+    res.redirect("/login");
+  });
+
+// **************************************
+
+
+// Create & submit a critter post
 app.post("/submit", (req, res) => {
     const { post_text } = req.body   
     const today_date = new Date();
@@ -45,6 +78,7 @@ app.post("/submit", (req, res) => {
     res.redirect("/posts")
 })
 
+// Delete the last post
 app.post("/delete", (req, res) => {
     POSTS.pop()
     res.redirect("/")
@@ -63,7 +97,8 @@ app.use(upload_photo.single("file"))
 
 
 
-
+// Connections
+mongoose.connect("mongodb://localhost/backend1")
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`)
 })
