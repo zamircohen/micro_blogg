b17363adbc159c9860f0d2ab9eef08f83334d5cc
Zamir Cohen <90824610+zamircohen@users.noreply.github.com>
2022-02-27 14:41:26 +0100

    Created a model for blogg entries

--- models/entries.js
+++ models/entries.js
@@ -1,9 +1,15 @@
 const mongoose = require("mongoose");
 const passportLocalMongoose = require("passport-local-mongoose");
 
-const entriesSchema = new mongoose.Schema({});
+const entriesSchema = new mongoose.Schema(
+    {
+        entry: String, 
+        entryBy: String,
+        entryDate: {type: Date, default: Date.now}
+});
+
+
 entriesSchema.plugin(passportLocalMongoose);
 
 const Entries = mongoose.model("Entries", entriesSchema);
-
 exports.Entries = Entries;
